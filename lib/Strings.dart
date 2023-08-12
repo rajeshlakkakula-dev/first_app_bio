479d02292a0e9747aa8ea50682c7b58c6435a0a2
Rajesh Lakkakula <rajeshlakkakula536@gmail.com>
2023-08-12 12:34:46 +0530

    Initial commit

---
+++ lib/Strings.dart
@@ -0,0 +1,10 @@
+
+
+
+class Strings{
+
+  static String name = "Dhoni Biography";
+  static String para_one =  "M.S. Dhoni, in full Mahendra Singh Dhoni, (born July 7, 1981, Ranchi, Bihar [now Jharkhand] state, India), Indian cricketer whose rise to prominence in the early 21st century culminated in his captaincy of the Indian national team that won the one-day Cricket World Cup in 2011.";
+  static String para_two= "Dhoni made his international debut in 2004. His talent with the bat came to the fore in an innings of 148 runs against Pakistan in his fifth international match. Within a year he joined the India Test team, where he quickly established himself with a century (100 or more runs in a single innings) against Pakistan. Despite his inexperience, Dhoni took over the captaincy of the one-day side in 2007 and led India to the Twenty20 (T20) world title. Series wins over Australia and Sri Lanka, among others, moved India to the top of the International Cricket Council (ICC) Test rankings for the first time in December 2009. Dhoni was honoured for his play with the ICC One Day International Player of the Year Award in 2008 and 2009. In the 2011 one-day World Cup, Dhoni’s dashing innings of 91 not out—in front of a home crowd in Mumbai—paved the way for India’s victory over Sri Lanka in the final. He also led India to an appearance in the semifinals of the 2015 Cricket World Cup. Dhoni stopped serving as India’s captain in 2017, having led his country in 331 international matches, the most for a captain in the sport’s history.";
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'Strings.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Mahi Bio"),
+            ),
+            body: SingleChildScrollView(
+                child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  child: Text(
+                    Strings.name,
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+                Image.asset(
+                  'images/dhoni.png',
+                  width: 350,
+                  height: 250,
+                ),
+                Padding(padding: EdgeInsets.only(bottom: 50)),
+                Text(Strings.para_one ,style: TextStyle(fontSize: 14, fontStyle: FontStyle.normal, color: Colors.black),textAlign: TextAlign.justify),
+                Container(
+                  margin: EdgeInsets.only(left: 10,top: 15,right: 10, bottom: 5),
+                    child: Text(Strings.para_two ,style: TextStyle(fontSize: 14, fontStyle: FontStyle.normal, color: Colors.black),textAlign: TextAlign.justify ),
+                ),
+
+              ],
+            ))));
+  }
+}
